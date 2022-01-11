6f74db6e8dcda5c1cba8eb16b85292a175784b67
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-11 15:28:43 -0800

    added auth form

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -1,3 +1,13 @@
 export default function AuthForm() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <label>Email:</label>
+        <input type="email" />
+        <label>Password:</label>
+        <input type="password" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
