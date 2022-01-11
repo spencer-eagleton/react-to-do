8b61cef7ceaed6045798cb8eefe0d865853640ac
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-11 14:32:06 -0800

    added browser router components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
