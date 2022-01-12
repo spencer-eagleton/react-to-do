63a9cacd12eb8fa0a006f39dacda93df11f4eeb5
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-12 10:37:30 -0800

    new task form

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,13 @@
-export default function Home() {
+export default function Home({ handleSubmit, newTask, setNewTask }) {
   return (
     <div>
       <h1>Your to-do list</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>New Task</label>
+        <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
