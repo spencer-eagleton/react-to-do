439729341efd2947c4216ac418be5fee0184c737
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-13 12:05:11 -0800

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders email text', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.getByText(/Email/i);
   expect(linkElement).toBeInTheDocument();
 });
